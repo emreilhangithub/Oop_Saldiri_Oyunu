6d576cb8abd963470a60f3a4d2e5eba62e362168
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-06-21 15:37:43 +0300

    Add project files.

---
+++ Oop_Saldiri_Oyunu/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace Oop_Saldiri_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtOyuncuAdi = new DevExpress.XtraEditors.TextEdit();
+            this.txtOyuncuIrk = new DevExpress.XtraEditors.TextEdit();
+            this.textOyuncuBrans = new DevExpress.XtraEditors.TextEdit();
+            this.txtOyuncuSilah = new DevExpress.XtraEditors.TextEdit();
+            this.btnSec = new DevExpress.XtraEditors.SimpleButton();
+            this.btnSaldir = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.txtOyuncuAdi.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtOyuncuIrk.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textOyuncuBrans.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtOyuncuSilah.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtOyuncuAdi
+            // 
+            this.txtOyuncuAdi.Location = new System.Drawing.Point(127, 12);
+            this.txtOyuncuAdi.Name = "txtOyuncuAdi";
+            this.txtOyuncuAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtOyuncuAdi.TabIndex = 0;
+            // 
+            // txtOyuncuIrk
+            // 
+            this.txtOyuncuIrk.Location = new System.Drawing.Point(127, 38);
+            this.txtOyuncuIrk.Name = "txtOyuncuIrk";
+            this.txtOyuncuIrk.Size = new System.Drawing.Size(100, 20);
+            this.txtOyuncuIrk.TabIndex = 1;
+            // 
+            // textOyuncuBrans
+            // 
+            this.textOyuncuBrans.Location = new System.Drawing.Point(127, 64);
+            this.textOyuncuBrans.Name = "textOyuncuBrans";
+            this.textOyuncuBrans.Size = new System.Drawing.Size(100, 20);
+            this.textOyuncuBrans.TabIndex = 2;
+            // 
+            // txtOyuncuSilah
+            // 
+            this.txtOyuncuSilah.Location = new System.Drawing.Point(127, 90);
+            this.txtOyuncuSilah.Name = "txtOyuncuSilah";
+            this.txtOyuncuSilah.Size = new System.Drawing.Size(100, 20);
+            this.txtOyuncuSilah.TabIndex = 3;
+            // 
+            // btnSec
+            // 
+            this.btnSec.Location = new System.Drawing.Point(279, 25);
+            this.btnSec.Name = "btnSec";
+            this.btnSec.Size = new System.Drawing.Size(75, 23);
+            this.btnSec.TabIndex = 4;
+            this.btnSec.Text = "Seç";
+            // 
+            // btnSaldir
+            // 
+            this.btnSaldir.Location = new System.Drawing.Point(152, 137);
+            this.btnSaldir.Name = "btnSaldir";
+            this.btnSaldir.Size = new System.Drawing.Size(75, 23);
+            this.btnSaldir.TabIndex = 5;
+            this.btnSaldir.Text = "Saldır";
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.BackColor = System.Drawing.Color.DarkRed;
+            this.labelControl1.Appearance.Options.UseBackColor = true;
+            this.labelControl1.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.labelControl1.Location = new System.Drawing.Point(469, 25);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(300, 30);
+            this.labelControl1.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(469, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 105);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(31, 15);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(63, 13);
+            this.labelControl2.TabIndex = 9;
+            this.labelControl2.Text = "Oyuncu İsmi:";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(31, 41);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(17, 13);
+            this.labelControl3.TabIndex = 10;
+            this.labelControl3.Text = "Irk:";
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Location = new System.Drawing.Point(31, 67);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(31, 13);
+            this.labelControl4.TabIndex = 11;
+            this.labelControl4.Text = "Brans:";
+            // 
+            // labelControl5
+            // 
+            this.labelControl5.Location = new System.Drawing.Point(31, 93);
+            this.labelControl5.Name = "labelControl5";
+            this.labelControl5.Size = new System.Drawing.Size(26, 13);
+            this.labelControl5.TabIndex = 12;
+            this.labelControl5.Text = "Silah:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 192);
+            this.Controls.Add(this.labelControl5);
+            this.Controls.Add(this.labelControl4);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.btnSaldir);
+            this.Controls.Add(this.btnSec);
+            this.Controls.Add(this.txtOyuncuSilah);
+            this.Controls.Add(this.textOyuncuBrans);
+            this.Controls.Add(this.txtOyuncuIrk);
+            this.Controls.Add(this.txtOyuncuAdi);
+            this.Name = "Form1";
+            this.Text = "Saldırı Oyunu";
+            ((System.ComponentModel.ISupportInitialize)(this.txtOyuncuAdi.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtOyuncuIrk.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textOyuncuBrans.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtOyuncuSilah.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.TextEdit txtOyuncuAdi;
+        private DevExpress.XtraEditors.TextEdit txtOyuncuIrk;
+        private DevExpress.XtraEditors.TextEdit textOyuncuBrans;
+        private DevExpress.XtraEditors.TextEdit txtOyuncuSilah;
+        private DevExpress.XtraEditors.SimpleButton btnSec;
+        private DevExpress.XtraEditors.SimpleButton btnSaldir;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.LabelControl labelControl5;
+    }
+}
+
---
+++ Oop_Saldiri_Oyunu/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors; //DevExpress dahil ettik
+
+namespace Oop_Saldiri_Oyunu
+{
+    public partial class Form1 : XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
